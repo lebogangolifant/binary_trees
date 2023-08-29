114838eec668c5428d9f0d87fea7161f7b080210
Lebogang <lebogangodeveloper@gmail.com>
2023-08-29 16:46:22 +0200

    Add functions prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -27,6 +27,8 @@ struct binary_tree_s
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /* Helper Functions */
 void binary_tree_print(const binary_tree_t *);
@@ -59,5 +61,6 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
 
 #endif /* BINARY_TREES_H */

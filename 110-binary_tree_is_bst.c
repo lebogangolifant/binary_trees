b319425fbd9a88c8b5544c713bc6395cf0321399
Lebogang <lebogangodeveloper@gmail.com>
2023-08-29 14:34:15 +0200

    Check if a binary tree is a valid

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,23 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_bst_recursive - Checks if a binary tree is a valid BST (recursive helper).
+ * binary_tree_is_bst_recursive - Checks if a binary tree is a valid BST
  * @tree: Pointer to the root node of the tree to check.
  * @min: Minimum value allowed in the tree.
  * @max: Maximum value allowed in the tree.
  *
  * Return: 1 if tree is a valid BST, 0 otherwise.
  */
+
 int binary_tree_is_bst_recursive(const binary_tree_t *tree, int min, int max)
 {
-    if (tree == NULL)
-        return (1);
+	if (tree == NULL)
+		return (1);
 
-    if (tree->n <= min || tree->n >= max)
-        return (0);
+	if (tree->n <= min || tree->n >= max)
+		return (0);
 
-    return (binary_tree_is_bst_recursive(tree->left, min, tree->n) &&
-            binary_tree_is_bst_recursive(tree->right, tree->n, max));
+	return (binary_tree_is_bst_recursive(tree->left, min, tree->n) &&
+			binary_tree_is_bst_recursive
+			(tree->right, tree->n, max));
 }
 
 /**
@@ -26,8 +28,8 @@ int binary_tree_is_bst_recursive(const binary_tree_t *tree, int min, int max)
  *
  * Return: 1 if tree is a valid BST, 0 otherwise.
  */
+
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-    return binary_tree_is_bst_recursive(tree, INT_MIN, INT_MAX);
+	return binary_tree_is_bst_recursive(tree, INT_MIN, INT_MAX);
 }
-

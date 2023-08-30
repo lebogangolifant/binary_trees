e131f2a7cf1431d699bc0e9a28294bb13bff3d69
Lebogang <lebogangodeveloper@gmail.com>
2023-08-30 16:04:14 +0200

    Perform a right-rotation on a binary tree

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -9,10 +9,10 @@
 
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *new_root = NULL;
+	binary_tree_t *new_root;
 
 	if (tree == NULL || tree->left == NULL)
-		return (NULL);
+		return (tree);
 
 	new_root = tree->left;
 	tree->left = new_root->right;
@@ -20,17 +20,8 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	if (new_root->right != NULL)
 		new_root->right->parent = tree;
 
-	new_root->parent = tree->parent;
-
-	if (tree->parent != NULL)
-	{
-		if (tree->parent->left == tree)
-			tree->parent->left = new_root;
-		else
-			tree->parent->right = new_root;
-	}
-
 	new_root->right = tree;
+	new_root->parent = tree->parent;
 	tree->parent = new_root;
 
 	return (new_root);
--- binary_trees.h
+++ binary_trees.h
@@ -75,5 +75,6 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 void linked_node(link_t **head, const binary_tree_t *tree, size_t level);
 int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
